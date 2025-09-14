934c0a537cbc9a155adff60d7be5fbe3ffdccff8
Ishavijay <ishavijay003@gmail.com>
2025-09-14 16:57:39 +0530

    Add files via upload

---
+++ lab1/address.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    printf("77,saraswati vihar-a,\n Jaipur, Rajasthan \n 302022");
+}
---
+++ lab1/sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter two number:");
+    scanf("%d %d",&a, &b);
+    c=a+b;
+    printf("the sum is: %d",c);
+
+    getchar();
+    getchar();
+    return 0;
+
+}
